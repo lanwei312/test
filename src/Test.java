a51ba5a2e567bb5540abfdce4a26fa67d1cdcbeb
bigbigMouthMonkey <lanwei312@163.com>
2021-02-25 23:19:28 +0800

    对象加到list,对象再改变，list中的值也会改变

--- src/Test.java
+++ src/Test.java
@@ -15,11 +15,25 @@ public class Test {
         List<Org> orgList = new ArrayList<>();
         orgList.add(org);
         System.out.println(orgList.get(0).getOrgId()+"-"+orgList.get(0).getParentId()+"-"+orgList.get(0).getOrgName());
+        //  1-0-张三
 
-        org.setOrgName("李四");
-        map.put(org.getOrgId(), org);
+        org.setOrgName("张三_new");
+        //map.put(org.getOrgId(), org);
         System.out.println(orgList.get(0).getOrgId()+"-"+orgList.get(0).getParentId()+"-"+orgList.get(0).getOrgName());
+        // 1-0-张三_new
 
+        Org org2 = new Org();
+        org2.setOrgId(2);
+        org2.setParentId(0);
+        org2.setOrgName("李四");
+        List<Org> orgList2 = new ArrayList<>();
+        orgList2.add(org2);
+        System.out.println(orgList2.get(0).getOrgId()+"-"+orgList2.get(0).getParentId()+"-"+orgList2.get(0).getOrgName());
+        // 2-0-李四
+
+        org2.setOrgName("李四_new");
+        System.out.println(orgList2.get(0).getOrgId()+"-"+orgList2.get(0).getParentId()+"-"+orgList2.get(0).getOrgName());
+        // 2-0-李四_new
     }
 
 
