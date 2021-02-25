1cdd9c2aa698fdc1181cd9c7fad990f448216e12
bigbigMouthMonkey <lanwei312@163.com>
2021-02-25 23:25:45 +0800

    map除了基本类型是正儿八经的值传递，其余的都是引用地址传递，地址没变，内容变化，也会引起调用该对象的其他内容的变化。

--- src/Test.java
+++ src/Test.java
@@ -34,6 +34,17 @@ public class Test {
         org2.setOrgName("李四_new");
         System.out.println(orgList2.get(0).getOrgId()+"-"+orgList2.get(0).getParentId()+"-"+orgList2.get(0).getOrgName());
         // 2-0-李四_new
+
+        /*
+        map除了基本类型是正儿八经的值传递，其余的都是引用地址传递
+
+        由于map的value存储的引用地址传递（list），所以当list对象的内部属性发生改变时，map中的value随着改变
+
+        所以可得：对象传的是引用地址，地址没变，内容变化，也会引起调用该对象的其他内容的变化。
+
+        Java中的对象变量 其实就中一个指针，他指向对象的首地址，map中key对应的其实是某个对象的首地址，指向的是某个对象，
+        你往对象中加了数据，对象的内容变了，但是对象的首地址没有变，map中的key仍然指向的是这个对象的首地址。
+        */
     }
 
 
